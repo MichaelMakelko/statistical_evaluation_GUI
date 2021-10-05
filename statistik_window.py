c8f57d9fdb249741b665ba7bd98080955c2c158b
Michael Makelko <76708703+MichaelMakelko@users.noreply.github.com>
2021-10-05 20:31:04 +0200

    Added new Funcions
    - Heatmap
    - scatterplot with a regression line

--- statistik_window.py
+++ statistik_window.py
@@ -153,11 +153,11 @@ def Median():
 
 
 # diese Funktion importiert den aktuellen zusammengelegten DataFrame
+# eine andere Mögliochkeit direkter zu importieren besteht nicht
 def df_for_statistic():
     from kalender_window import df_date
     from datensatz_window import df_gui
     merged_df = pd.merge(df_date, df_gui , left_index=True, right_index=True)
-    print("jetzt in der import merged_df Funktion")
     print(merged_df)
     return merged_df
     
@@ -220,6 +220,103 @@ def newselection2(event):
     if compare == "Monate":
         boxMonate()
         
+def scatter_regression():
+    df = df_for_statistic()
+    save_string = df.columns[1]
+
+    # Funktion konvertiert die datetime in float datentypen
+    def dt64_to_float(dt64):
+        """Converts numpy.datetime64 to year as float.
+        Rounded to days
+        Parameters
+        ----------
+        dt64 : np.datetime64 or np.ndarray(dtype='datetime64[X]')
+            date data
+        Returns
+        -------
+        float or np.ndarray(dtype=float)
+            Year in floating point representation
+        """
+        year = dt64.astype('M8[Y]')
+        # print('year:', year)
+        days = (dt64 - year).astype('timedelta64[D]')
+        # print('days:', days)
+        year_next = year + np.timedelta64(1, 'Y')
+        # print('year_next:', year_next)
+        days_of_year = (year_next.astype('M8[D]') - year.astype('M8[D]')
+                        ).astype('timedelta64[D]')
+        # print('days_of_year:', days_of_year)
+        dt_float = 1970 + year.astype(float) + days / (days_of_year)
+        # print('dt_float:', dt_float)
+        return dt_float
+
+
+    # print(df["longtime"].dtype)
+    # dates = pd.DataFrame([])
+    # #df.reset_index(drop=True, inplace=True)
+    # for i in df["longtime"]:
+    #     stamp = pd.Timestamp(i).timestamp()
+    #     dates.append(stamp)
+
+        
+    # print(dates)
+
+
+    #df["timestamp"] = df["longtime"].to_timestamp()
+    df["date_float"] = dt64_to_float(df["longtime"].to_numpy())
+    #print(new)
+    #print(df)
+
+    # darstellen des Streuungsidagramm sowie einer Regressionslinie
+    sns.regplot(x=df["date_float"], y=df[save_string])
+    plt.show()
+
+
+def heatmap():
+    df = df_for_statistic()
+    df = df.dropna()
+    # setze den column von "longtime" als index
+    df = df.set_index(df["longtime"])
+    # lösche des "longtime" columns aus dem dataframe
+    df = df.drop(["longtime"], axis=1)
+    
+    # erstelle ein column für die stunden/minute/sekunde
+    df['hour'] = df.index.strftime('%H:%M:%S')
+    # erstelle ein column für die jahr/monat/tag
+    df['dayofyear'] = df.index.date
+
+    # erstellen einer Tabelle mit füllen der Temperaturwerten / Heatmap Einstellungen
+    df = df.pivot(index='hour', columns='dayofyear', values=df.columns[0])
+
+    yticks_spacing = int(pd.Timedelta("2h")/pd.Timedelta("30s"))
+    ax = sns.heatmap(df, yticklabels=yticks_spacing, xticklabels=2, linewidths=.1, linecolor="#222", cmap="icefire")
+    plt.title("Heatmap", fontsize = 20) # title with fontsize 20
+    plt.xlabel('Tage', fontsize = 15) # x-axis label with fontsize 15
+    plt.ylabel('Uhrzeit', fontsize = 15) # y-axis label with fontsize 15
+    plt.yticks(rotation=0)
+    plt.show()
+
+
+    # AUS STACKOVERFLOW
+    # freq = '30s'
+
+    # #df = pd.DataFrame(np.random.randint(10, size=4*24*200*20))
+    # df.index = pd.date_range(start=df["longtime"], periods=200*24*4*20, freq=freq)
+
+    # df['hour'] = df.index.strftime('%H:%M:%S')
+    # df['dayofyear'] = df.index.date
+
+
+    # df = df.pivot(index='dayofyear', columns='hour', values=df.columns[1])
+    # df.columns = pd.DatetimeIndex(df.columns).strftime('%H:%M')
+    # df.index = pd.DatetimeIndex(df.index).strftime('%m/%Y')
+
+    # xticks_spacing = int(pd.Timedelta('2h')/pd.Timedelta(freq))
+    # ax = sns.heatmap(df, xticklabels=xticks_spacing, yticklabels=30)
+    # plt.yticks(rotation=0)
+    # plt.show()
+    
+    print(df)
 
 
 def open_statistik():
@@ -258,6 +355,24 @@ def open_statistik():
     cb2.grid(column=0, row=1, padx=5, pady=5)
     cb2.bind("<<ComboboxSelected>>", newselection2)
 
+    #---------------------------------------------------------------------------------------------#
+    frame_statistik = ttk.LabelFrame(statistik, text="Weiteres")
+    frame_statistik.pack(side=TOP, padx=10, pady=10)
+    #---------------------------------------------------------------------------------------------#
+    # STREUUNGSDIAGRAMM TENDENZLINIE
+    fr = ttk.Frame(frame_statistik)
+    fr.pack(side=TOP, padx=10, pady=10)
+    #---------------------------------------------------------------------------------------------#
+    StreuButton = ttk.Button(fr, text="scatter_regression", command=scatter_regression)
+    StreuButton.grid(column=0 , row=0 , padx=5, pady=5)
+
+    heatButton = ttk.Button(fr, text="Heatmap", command=heatmap)
+    heatButton.grid(column=0 , row=1 , padx=5, pady=5)
+
+
+    #---------------------------------------------------------------------------------------------#
+    frame_statistik = ttk.LabelFrame(statistik)
+    frame_statistik.pack(side=TOP, padx=10, pady=10)
     #---------------------------------------------------------------------------------------------#
     fr = ttk.Frame(frame_statistik)
     fr.pack(side=TOP, padx=10, pady=10)
